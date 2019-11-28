0f3dca6d6025f85e2c7186a2e67d341a0768dac1
hsimah <hamishblake@gmail.com>
2019-11-28 10:01:05 -0800

    create plugin class
    create plugin class with simple number field sample

---
+++ class-metabox.php
@@ -0,0 +1,118 @@
+<?php
+
+use WPGraphQL\Data\DataSource;
+use WPGraphQL\Data\Connection\PostObjectConnectionResolver;
+
+final class WPGraphQL_MetaBox {
+
+    /**
+     * Stores the instance of the WPGraphQL_MetaBox class
+     *
+     * @var WPGraphQL_MetaBox The one true WPGraphQL_MetaBox
+     * @since  0.0.1
+     * @access private
+     */
+    private static $instance;
+
+    /**
+     * The instance of the WPGraphQL_MetaBox object
+     *
+     * @return object|WPGraphQL_MetaBox - The one true WPGraphQL_MetaBox
+     * @since  0.0.1
+     * @access public
+     */
+    public static function instance() {
+
+        if ( ! isset( self::$instance ) && ! ( self::$instance instanceof WPGraphQL_MetaBox ) ) {
+            self::$instance = new WPGraphQL_MetaBox();
+            self::$instance->init();
+        }
+
+        /**
+         * Return the WPGraphQL_MetaBox Instance
+         */
+        return self::$instance;
+    }
+
+    /**
+     * Throw error on object clone.
+     * The whole idea of the singleton design pattern is that there is a single object
+     * therefore, we don't want the object to be cloned.
+     *
+     * @since  0.0.1
+     * @access public
+     * @return void
+     */
+    public function __clone() {
+
+        // Cloning instances of the class is forbidden.
+        _doing_it_wrong( __FUNCTION__, esc_html__( 'The WPGraphQL_MetaBox class should not be cloned.', 'wpgraphiql-mb-relationships' ), '0.0.1' );
+
+    }
+
+    /**
+     * Disable unserializing of the class.
+     *
+     * @since  0.0.1
+     * @access protected
+     * @return void
+     */
+    public function __wakeup() {
+
+        // De-serializing instances of the class is forbidden.
+        _doing_it_wrong( __FUNCTION__, esc_html__( 'De-serializing instances of the WPGraphQL_MetaBox class is not allowed', 'wpgraphiql-mb-relationships' ), '0.0.1' );
+
+    }
+
+    public static function register_field( $field, $type, $object_type ) {
+        // TODO support other types
+        if ( 'post' !== $object_type ) return;
+        
+        $post_type_object = get_post_type_object( $type );
+    
+        // check this type and field should be exposed in the schema
+        if ( $post_type_object->show_in_graphql && $field['graphql_name'] ) {
+            register_graphql_fields( $post_type_object->graphql_single_name, [
+                $field['graphql_name'] => [
+                    'type' => 'Int', // TODO add type resolver for other types
+                    'description' => $field['name'],
+                    'resolve' => function( $post ) {
+                        return rwmb_meta( $field['id'], null, $post->ID );
+                    },
+                ]
+            ] );
+        }
+    }
+
+    /**
+     * Initialise plugin.
+     *
+     * @access private
+     * @since  0.0.1
+     * @return void
+     */
+    private function init() {
+
+      add_action( 'rwmb_field_registered', [ 'WPGraphQL_MetaBox', 'register_connection' ], 1 );
+
+    }
+
+}
+
+// TODO find appropriate action to hook into
+add_action( 'init', 'WPGraphQL_MetaBox_init' );
+
+if ( ! function_exists( 'WPGraphQL_MetaBox_init' ) ) {
+    /**
+     * Function that instantiates the plugins main class
+     *
+     * @since 0.0.1
+     */
+    function WPGraphQL_MetaBox_init() {
+
+        /**
+         * Return an instance of the action
+         */
+        return \WPGraphQL_MetaBox::instance();
+    }
+}
---
+++ wp-graphql-metabox.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Plugin Name: WP GraphQL Meta Box Integration
+ * Plugin URI: https://github.com/hsimah/wp-graphql-metabox
+ * Description: WP GraphQL provider for Meta Box
+ * Author: hsimah
+ * Author URI: http://www.hsimah.com
+ * Version: 0.0.1
+ * Text Domain: wpgraphql-metabox
+ * License: GPL-3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @package  WPGraphQL_MetaBox
+ * @author   hsimah
+ * @version  0.1.0
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'WPGraphQL_MetaBox' ) ) {
+
+	add_action( 'admin_init', function () {
+		$wp_graphql_required_min_version = '0.3.2';
+
+		if ( ! class_exists( 'MBR_Loader' ) || ! class_exists( 'WPGraphQL' ) || ( defined( 'WPGRAPHQL_VERSION' ) && version_compare( WPGRAPHQL_VERSION, $wp_graphql_required_min_version, 'lt' ) ) ) {
+	
+			/**
+			 * For users with lower capabilities, don't show the notice
+			 */
+			if ( ! current_user_can( 'manage_options' ) ) {
+				return false;
+			}
+	
+			add_action(
+				'admin_notices',
+				function() use ( $wp_graphql_required_min_version ) {
+					?>
+				<div class="error notice">
+					<p>
+						<?php _e( sprintf('Both WPGraphQL (v%s+) and Meta Box (v3.3.9) must be active for "wp-graphql-metabox" to work', $wp_graphql_required_min_version ), 'wpgraphql-metabox' ); ?>
+					</p>
+				</div>
+					<?php
+				}
+			);
+	
+			return false;
+		}
+	} );
+
+	if ( class_exists( 'MBR_Loader' ) && class_exists( 'WPGraphQL' ) )
+		require_once __DIR__ . '/class-metabox.php';
+
+}
