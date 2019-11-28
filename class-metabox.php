2d140071f429fbd5a1e01d76beb991c2b4148aad
hsimah <hamishblake@gmail.com>
2019-11-28 10:19:39 -0800

    add initial type resolver

--- class-metabox.php
+++ class-metabox.php
@@ -64,6 +64,13 @@ final class WPGraphQL_MetaBox {
 
     }
 
+    /**
+     * Register Meta Box field with WPGraphQL
+     *
+     * @since  0.0.1
+     * @access protected
+     * @return void
+     */
     public static function register_field( $field, $type, $object_type ) {
         // TODO support other types
         if ( 'post' !== $object_type ) return;
@@ -74,13 +81,50 @@ final class WPGraphQL_MetaBox {
         if ( $post_type_object->show_in_graphql && $field['graphql_name'] ) {
             register_graphql_fields( $post_type_object->graphql_single_name, [
                 $field['graphql_name'] => [
-                    'type' => 'Int', // TODO add type resolver for other types
+                    'type' => self::resolve_graphql_type( $field['type'] ),
                     'description' => $field['name'],
                     'resolve' => function( $post ) {
                         return rwmb_meta( $field['id'], null, $post->ID );
                     },
                 ]
             ] );
+        }
+    }
+
+    /**
+     * Resolve Meta Box type to GraphQL type
+     *
+     * @since  0.0.1
+     * @access protected
+     * @return string
+     */
+    private static function resolve_graphql_type( $field_type ) {
+        switch ( $field_type ) {
+            case 'custom_html':
+            case 'url':
+            case 'datetime':
+            case 'text':
+            case 'wysiwyg':
+                return 'String';
+            case 'number':
+                // TODO int or float?
+                return 'Int';
+            case 'range':
+                // TODO int or float?
+                return 'Float';
+            case 'checkbox':
+            case 'switch':
+                return 'Boolean';
+            case 'taxonomy':
+                // TODO
+                return;
+            case 'group':
+                // TODO
+                return;
+            case 'post':
+            case 'user':
+                // this should be a connection
+                return;
         }
     }
 
@@ -93,7 +137,7 @@ final class WPGraphQL_MetaBox {
      */
     private function init() {
 
-      add_action( 'rwmb_field_registered', [ 'WPGraphQL_MetaBox', 'register_connection' ], 1 );
+      add_action( 'rwmb_field_registered', [ 'WPGraphQL_MetaBox', 'register_connection' ], 10, 3 );
 
     }
 
